f7150c44af915c79bb915f5dbb7f29559ec97556
soojin <SeungWoo_0914@Seungs-MBP-2.attlocal.net>
2019-07-08 21:58:04 -0700

    first commit

---
+++ 3dtext/3dtext/ViewController.swift
@@ -0,0 +1,84 @@
+//
+//  ViewController.swift
+//  3dtext
+//
+//  Created by Seungwoo Lee on 7/8/19.
+//  Copyright © 2019 Soojin Shim. All rights reserved.
+//
+
+import UIKit
+import SceneKit
+import ARKit
+
+class ViewController: UIViewController, ARSCNViewDelegate {
+
+    @IBOutlet var sceneView: ARSCNView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set the view's delegate
+        sceneView.delegate = self
+        
+        let textGeometry = SCNText(string:"No rain No flowers", extrusionDepth: 1)
+        
+        let material = SCNMaterial()
+        material.diffuse.contents = UIColor.white
+        textGeometry.font = UIFont(name: "Brandon Grotesque", size: 17)
+        textGeometry.materials = [material]
+        
+        textGeometry.flatness = 0
+        
+        
+        let node = SCNNode()
+        node.position = SCNVector3(x: 0, y:0.02, z: -0.1)
+        node.scale = SCNVector3(x: 0.01, y: 0.01, z: 0.01)
+        node.geometry = textGeometry
+        
+        sceneView.scene.rootNode.addChildNode(node)
+        sceneView.autoenablesDefaultLighting = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Create a session configuration
+        let configuration = ARWorldTrackingConfiguration()
+
+        // Run the view's session
+        sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        // Pause the view's session
+        sceneView.session.pause()
+    }
+
+    // MARK: - ARSCNViewDelegate
+    
+/*
+    // Override to create and configure nodes for anchors added to the view's session.
+    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        let node = SCNNode()
+     
+        return node
+    }
+*/
+    
+    func session(_ session: ARSession, didFailWithError error: Error) {
+        // Present an error message to the user
+        
+    }
+    
+    func sessionWasInterrupted(_ session: ARSession) {
+        // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        
+    }
+    
+    func sessionInterruptionEnded(_ session: ARSession) {
+        // Reset tracking and/or remove existing anchors if consistent tracking is required
+        
+    }
+}
---
+++ 3dtext/3dtextTests/_dtextTests.swift
@@ -0,0 +1,34 @@
+//
+//  _dtextTests.swift
+//  3dtextTests
+//
+//  Created by Seungwoo Lee on 7/8/19.
+//  Copyright © 2019 Soojin Shim. All rights reserved.
+//
+
+import XCTest
+@testable import _dtext
+
+class _dtextTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
